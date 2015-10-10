806ec18bd638399ee7aafdcb26a5c0451a96597e
Yu Wu <wuyucsu@gmail.com>
2015-10-10 11:16:43 -0500

    rewrite pointcut expression with @Pointcut

--- src/main/java/com/mtjwy/spring/concert/Audience.java
+++ src/main/java/com/mtjwy/spring/concert/Audience.java
@@ -4,32 +4,36 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 
 @Aspect
 public class Audience {
 
+	//Declaring a frequently used pointcut expression with @Pointcut
+	@Pointcut("execution(* com.mtjwy.spring.concert.Performance.perform(..))")
+	public void perform() {}
 	
 	//The advice method is called before the advised method is called.
-	@Before("execution(* com.mtjwy.spring.concert.Performance.perform(..))")
+	@Before("perform()")
 	public void silenceCellPhones() {
 		System.out.println("Silencing cell Phones");
 	}
 	
-	@Before("execution(* com.mtjwy.spring.concert.Performance.perform(..))")
+	@Before("perform()")
 	public void takeSeats() {
 		System.out.println("Taking seats");
 	}
 	
 	//The advice method is called after the advised method returns.
-	@AfterReturning("execution(* com.mtjwy.spring.concert.Performance.perform(..))")
+	@AfterReturning("perform()")
 	public void applause() {
 		System.out.println("CLAP CLAP CLAP!!!");
 	}
 	
 	//The advice method is called after the advised method throws an exception.
-	@AfterThrowing("execution(* com.mtjwy.spring.concert.Performance.perform(..))")
+	@AfterThrowing("perform()")
 	public void demandRefund() {
 		System.out.println("Demanding a refund");
 	}
